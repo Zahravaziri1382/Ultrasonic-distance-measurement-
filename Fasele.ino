178b01e1364f41af2c9f23b6c24c6b6f2a74b2a6
Zahravaziri1382 <167520937+Zahravaziri1382@users.noreply.github.com>
2024-04-27 13:45:27 +0330

    Add files via upload

---
+++ Fasele.ino
@@ -0,0 +1,26 @@
+long duration;
+int distance;
+const int trig=9;
+const int echo=10;
+void setup() {
+pinMode(trig,OUTPUT);
+pinMode(echo,INPUT);
+Serial.begin(9600);
+// put your setup code here, to run once:
+
+}
+
+void loop() {
+ distance= calculateDistance();
+ Serial.println(distance);
+} 
+int calculateDistance(){
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+    delayMicroseconds(10);
+     digitalWrite(trig,LOW);
+     duration=pulseIn(echo,HIGH);
+     distance=duration*0.034/2;
+     return distance; 
+}
